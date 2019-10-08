031ef78292e93316107cc904a288421c8976c8ee
Alisha Saley <amsaley@ucdavis.edu>
2019-10-08 12:52:12 -0700

    Update Sigmoidal curve.R
    alisha deleted a chunk

--- Sigmoidal curve.R
+++ Sigmoidal curve.R
@@ -2,19 +2,7 @@
 b=0
 m=1
 x=seq(from=-10, to=10, by =0.1)
-y=m*x+b
-plot(y~x) ##We now have a straight line and we can backtransform it into proportional space
-plot(plogis(y)~x) ###This works and we now have a logistic curve
-
-##What happens if we change the intercept?
-###We are now going to create a bunch of curves with differing intercepts and keep the slope constant
-###Need to create a data frame with different intercepts
-b=seq(-5, 5, 1) #We will be looking at values of b from -5 to 5, so we should have 11 lines total
-y=NULL
-# y=rep(0, length(x)*length(b)) #This creates an empty vector we will populate with y values with differing intercepts. The total number of values of lines equals the number of x values we have multiplied by the number of intercept values we have
-for(i in 1:length(b)){
-	y1=m*x+b[i]
-	y=c(y, y1)
+y=
 }
 <<<<<<< HEAD
 =======
