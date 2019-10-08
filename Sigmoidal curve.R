c41f9ac3c4309be7b614210ab123b3338b8cae04
Gabriel Ng <gng@ucdavis.edu>
2019-10-08 11:57:38 -0700

    Update Sigmoidal curve.R

--- Sigmoidal curve.R
+++ Sigmoidal curve.R
@@ -16,3 +16,33 @@ for(i in 1:length(b)){
 	y1=m*x+b[i]
 	y=c(y, y1)
 }
+
+####For slope
+##What happens if we change the intercept?
+###We are now going to create a bunch of curves with differing intercepts and keep the slope constant
+###Need to create a data frame with different intercepts
+b=0
+m=seq(-5, 5, by =0.02) #We will be looking at values of b from -5 to 5, so we should have 11 lines total
+y=NULL
+# y=rep(0, length(x)*length(b)) #This creates an empty vector we will populate with y values with differing intercepts. The total number of values of lines equals the number of x values we have multiplied by the number of intercept values we have
+for(i in 1:length(m)){
+	y1=m[i]*x+b
+	y=c(y, y1)
+}
+
+#Create data frame to expand b
+m.exp=NULL
+for(i in 1:length(m)){
+	m.exp.1=rep(m[i], length(x))
+	m.exp=c(m.exp, m.exp.1)
+}
+
+##Now let's create the data frame
+dat=data.frame(y=y, x=rep(x, length(m)), m=m.exp)
+head(dat)
+dat$prop=plogis(dat$y)
+
+plot(prop~x, data=dat, type='n')
+for(i in 1:length(unique(dat$m))){
+	lines(prop~x, data=dat[dat$m==unique(dat$m)[i],], col=i, lwd=3)
+}
